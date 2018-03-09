67bd7eea65e42442b10257c12695e8a27a42b767
cocofly <1543404677@qq.com>
2018-03-09 18:28:38 +0800

    智障一样的ide

---
+++ src/main/java/com/demo/constant/Constant.java
@@ -0,0 +1,11 @@
+package com.demo.constant;
+
+/**
+ * @author d-xsj
+ * mail coder_xushijia@126.com
+ * date 09/03/2018
+ * Description:
+ */
+public class Constant {
+    public static String wiki_vec_url = "/Users/a/Desktop/data/model/hanlp-wiki-vec-zh.txt";
+}
